82a0f10590dabc558b550bdd4aaf8ea240fb8d50
thiago guedes <tguedes6@hotmail.com>
2017-05-30 09:37:10 -0300

    criacao do card-header e inicio do menu

--- dribbble web/ApiDribbbleWeb/src/app/app.module.ts
+++ dribbble web/ApiDribbbleWeb/src/app/app.module.ts
@@ -5,14 +5,22 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { CardHomeComponent } from './util-components/card-home/card-home.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "app/app.routing.module";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardHomeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
