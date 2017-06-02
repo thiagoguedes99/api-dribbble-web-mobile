25be04d3a08257cf6de75803c2bf3ea0fce97ced
thiago guedes <tguedes6@hotmail.com>
2017-06-02 08:38:08 -0300

    correcao sticky bar

--- dribbble web/ApiDribbbleWeb/src/app/app.module.ts
+++ dribbble web/ApiDribbbleWeb/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// automactily component created
 import { AppComponent } from './app.component';
-
-import { CardHomeComponent } from './shared/card-home/card-home.component';
-import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from "app/app.routing.module";
 
-import { DribbbleServiceService } from "app/providers/dribbble-service.service";
+// import of tried
+import { StickyNavModule } from 'ng2-sticky-nav';
+
+// my components of view
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { CardHomeComponent } from './shared/card-home/card-home.component';
+
+// my components generics
 import { PersonalCardComponent } from './shared/personal-card/personal-card.component';
 import { SimpleCardComponent } from './shared/simple-card/simple-card.component';
-import { ProfileComponent } from './profile/profile.component';
+import { DetailShotsComponent } from './detail-shots/detail-shots.component';
+
+// my services
+import { DribbbleServiceService } from "app/providers/dribbble-service.service";
 
 @NgModule({
   declarations: [
@@ -21,12 +30,15 @@ import { ProfileComponent } from './profile/profile.component';
     HomeComponent,
     PersonalCardComponent,
     SimpleCardComponent,
-    ProfileComponent
+    ProfileComponent,
+    DetailShotsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+
+    StickyNavModule,
 
     AppRoutingModule,
   ],
---
+++ dribbble web/ApiDribbbleWeb/src/app/detail-shots/detail-shots.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-shots',
+  templateUrl: './detail-shots.component.html',
+  styleUrls: ['./detail-shots.component.css']
+})
+export class DetailShotsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
